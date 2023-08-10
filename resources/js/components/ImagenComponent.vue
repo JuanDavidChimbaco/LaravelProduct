4c94c32cf768e67d998035513689627c059af670
JuanDavidChimbaco <dajun318@gmail.com>
2023-08-10 18:47:12 -0500

    card

--- resources/js/components/ImagenComponent.vue
+++ resources/js/components/ImagenComponent.vue
@@ -2,7 +2,14 @@
     <div>
         <h1>Imagen</h1>
         {{ urlPokemon }}
-        <img :src="img" alt="">
+        <div  v-if="urlPokemon" class="d-flex justify-content-center">
+            <div class="card" style="width: 18rem;">
+                <img :src="img" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text" v-text="name"></p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,17 +18,17 @@
         data() {
             return {
                 img: '',
+                name: '',
             };
         },
         props: ['url-pokemon'],
         watch: {
             urlPokemon(){
-                alert(this.urlPokemon);
                 axios.get(this.urlPokemon)
                     .then((res) => {
                         console.log(res);
                         this.img = res.data.sprites.other["official-artwork"].front_default;
-
+                        this.name = res.data.name;
                     })
                     .catch((err) => {
                         console.error(err);
